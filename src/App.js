f4526cff3bd056840001f75b8355c7ac3ef5050d
Samuel Tamuno Tope <samueltamunotope@gmail.com>
2023-09-24 16:43:58 -0700

    upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import './index.css';
 
-function App() {
-  return (
+ function App (){
+  const [tasks, setTasks] =  useState([
+    {id: 5271, name: "Record First Song", completed: true},
+    {id:3457, name: "Record Second Song", completed: false},
+    {id:3458, name: "Record Third Song", completed: false}
+  ]);
+     
+  function deleteHandler(id) {
+    setTasks(tasks.filter(task => id !== task.id));
+
+   }
+   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className = "box">
+      <h1>Task List</h1>
+      <ul>
+        {tasks.map((task) =>(
+          <li key={task.id}>
+            <span>{task.id} - {task.name}</span>
+            <button onClick={() => deleteHandler(task.id)} className = "delete">Delete</button>
+            </li>
+        ))}
+      </ul>
+      </div>
     </div>
+
   );
 }
 
+
 export default App;
+
+
+
